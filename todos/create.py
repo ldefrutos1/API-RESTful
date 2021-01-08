420cbc88f32452161373b619118e2e156fb1bb34
EC2 Default User <ec2-user@ip-172-31-88-219.ec2.internal>
2021-01-08 11:38:33 +0000

    ramaso

--- todos/create.py
+++ todos/create.py
@@ -20,7 +20,7 @@ def create(event, context):
 
     item = {
         'id': str(uuid.uuid1()),
-        'text': data['text'] + "desde: new feature",
+        'text': data['text'] + "desde: la rama  new feature",
 
 
         'checked': False,
