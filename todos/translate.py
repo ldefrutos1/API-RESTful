91b134f9f4e5441e6266b66731498852f6fb7576
EC2 Default User <ec2-user@ip-172-31-88-219.ec2.internal>
2021-01-09 10:42:01 +0000

    .

--- todos/translate.py
+++ todos/translate.py
@@ -6,7 +6,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 
-def get(event, context):
+def translate(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
